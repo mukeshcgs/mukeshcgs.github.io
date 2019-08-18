def58bf18242f018305215edae784fc319b5a42a
mukeshcgs <mukeshcgs@gmail.com>
2019-08-18 16:46:25 +0530

    home commite

--- webpack.common.js
+++ webpack.common.js
@@ -94,9 +94,17 @@ module.exports = {
   },
 
   resolve: {
+    modules: ['node_modules'],
     alias: {
       modernizr$: path.resolve(__dirname, "private/.modernizrrc"),
       "barba": path.resolve('node_modules', 'barba.js/dist/barba.min'),
+      'TweenLite': 'gsap/src/minified/TweenLite.min.js',
+      'TweenMax': 'gsap/src/minified/TweenMax.min.js',
+      'TimelineLite': 'gsap/src/minified/TimelineLite.min.js',
+      'TimelineMax': 'gsap/src/minified/TimelineMax.min.js',
+      'ScrollMagic': 'scrollmagic/scrollmagic/minified/ScrollMagic.min.js',
+      'animation.gsap': 'scrollmagic/scrollmagic/minified/plugins/animation.gsap.min.js',
+      'debug.addIndicators': 'scrollmagic/scrollmagic/minified/plugins/debug.addIndicators.min.js'
     }
   },
 
