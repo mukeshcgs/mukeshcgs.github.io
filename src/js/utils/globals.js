bc9ec447c6a97db1038df53ded63951557bf31f9
mukesh <mukeshcgs@gmail.com>
2019-09-29 12:02:31 +0530

    Initial dist subtree commit

--- src/js/utils/globals.js
+++
@@ -1,9 +0,0 @@
-/* jshint esnext: true */
-// import PageTransitionsManager from '../modules/PageTransitionsManager';
-
-export default function() {
-	// window.Ractive.DEBUG = false;
-	// svg4everybody();
-
-	// var pagetransitionsManager = new PageTransitionsManager();
-}
