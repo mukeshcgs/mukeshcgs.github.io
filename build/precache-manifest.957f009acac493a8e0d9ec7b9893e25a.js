fcc33626e0a4e74ed6fcc1f083166143c31340e4
Thankar <mukesh.thankar@MBICWW-DMX00031.local>
2020-08-09 18:21:52 +0530

    bootstrap form formating added

---
+++ build/precache-manifest.957f009acac493a8e0d9ec7b9893e25a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c02f820025d505ef588439d708f2419c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "354c7612b14ca3f8100c",
+    "url": "/static/css/main.0f0e757b.chunk.css"
+  },
+  {
+    "revision": "6ee63d67881f9f0a4812",
+    "url": "/static/js/2.e9f0e50f.chunk.js"
+  },
+  {
+    "revision": "c87550333da8df8ab4c80bd1d6e9defb",
+    "url": "/static/js/2.e9f0e50f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "354c7612b14ca3f8100c",
+    "url": "/static/js/main.acb2d7e9.chunk.js"
+  },
+  {
+    "revision": "54c7a3cbba9303040614",
+    "url": "/static/js/runtime-main.5ee2c2d0.js"
+  }
+]);
