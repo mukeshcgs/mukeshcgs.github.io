9807ea73feaf09448da3d881f81efeade355935b
mukesh <mukeshcgs@gmail.com>
2020-08-20 00:24:49 +0530

    amfah initial commit

---
+++ amfah-react/precache-manifest.c665b54171a4edb3d00236c4bb9461f0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f7bb044b832c7bb498daeb26fafe2310",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fff77037b6b728fbd83a",
+    "url": "/static/css/main.1fd013ce.chunk.css"
+  },
+  {
+    "revision": "db52d8dca1098ad5a2fc",
+    "url": "/static/js/2.7ae4f49e.chunk.js"
+  },
+  {
+    "revision": "c87550333da8df8ab4c80bd1d6e9defb",
+    "url": "/static/js/2.7ae4f49e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fff77037b6b728fbd83a",
+    "url": "/static/js/main.a090af6d.chunk.js"
+  },
+  {
+    "revision": "832cdb04dcd13c55fbec",
+    "url": "/static/js/runtime-main.6eb28117.js"
+  }
+]);
