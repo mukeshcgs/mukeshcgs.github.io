4aac2b244fcf971a035d1dda627ce6e0a26d90a5
mukesh <mukeshcgs@gmail.com>
2020-09-17 19:00:52 +0530

    site sharing images addedd for Facebook and Twitter

---
+++ Grunt/build/dist/covid19/js/global.js
@@ -0,0 +1,9 @@
+// $(function () {
+//     $.get("../header.html", function (data) {
+//         $("header").html(data);
+//     });
+
+//     $.get("../aside.html", function (data) {
+//         $("aside").html(data);
+//     });
+// });
