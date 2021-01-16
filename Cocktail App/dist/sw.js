3a4973b7e4614e2c43803daf4b838f26ee75d292
mukesh <mukeshcgs@gmail.com>
2021-01-16 12:56:18 +0530

    Release at

--- Cocktail App/dist/sw.js
+++ Cocktail App/dist/sw.js
@@ -1,11 +1,14 @@
 self.addEventListener("install", e => {
     console.log("Installed decode 22");
     e.waitUntil(caches.open("static").then(cache => {
-            return cache.addAll(["./", "./css/main.min.css", "./js/app.min.js"])
-        }
-
-    ))
+        return cache.addAll(["./", "./css/main.min.css", "./js/app.min.js"])
+    }))
 })
+//activate event
+self.addEventListener('activate', e => {
+    console.log('Service worker has been activated');
+})
+
 self.addEventListener("fetch", e => {
     console.log(`Intercepting fetch request for : ${e.request.url}`);
     e.respondWith(
